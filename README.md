6bc3399db64302b0ba7faf88de589a3dcb34dd94
precursorca <support@precursor.ca>
2019-01-08 07:59:52 -0600

    Munki Stats Widget v.1.0.2
    A custom munkireport 4 Munki Stats Widget to show managed installs similar to the way they appeared in early versions of munkireport 2.

--- README.md
+++ README.md
@@ -1,34 +1,45 @@
 # munki-stats-widget
 
-> v. 1.0.1 
-> January 7, 2019  
+> v. 1.0.2 
+> January 8, 2019  
 > Alex Narvey / Precursor.ca  
 
 ![Munki Stats Widget](https://github.com/precursorca/munki-stats-widget/blob/master/munki_stats_widget.png)
 
-A custom MunkiReport 4.0 Stats Widget to show statistics similar to early versionss of Munki.
+A custom MunkiReport 4.0 Stats Widget to show statistics similar to early versions of Munki.
 
 No warrantee is offered. Neither express nor implied. Use at your own risk.
 
 To use:
 
-1) Place the munki_stats_widget.php file inside the local/views/widgets directory of MunkiReport 4
+1) Replace the function in managedinstalls_controller.php with the on in Custom Get_Stats.txt.
+(Thanks to John Eberle for providing this custom function)
 
-2) Call it from a dashboard or make a custom dashboard with a .yml file in /local/dashboards
+2) Place the munki_stats_widget.php file inside the local/views/widgets directory of MunkiReport 4
+
+3) Call it from a dashboard or make a custom dashboard with a .yml file in /local/dashboards
 
 Notes:
 
-- The stats come from the getStats function in the ManagedInstalls module.
-- The period is 30 days using:
-		var hours = 24 * 30;
+- The stats come from the get_Stats function in the ManagedInstalls module.
+- In this case John Eberle modified the Get_Stats function to help pick out the correct arrays with JQuery.
+- The period is 720 hours (one month).
 - The formatting is borrowed from the MS_Office module.
 - The Pending count is a total of Pending Munki Installs and Pending AppleSUS Installs
+- Arjen van Bochoven schooled me on dealing with null array elements
 
+
+* January 8, 2019 Version 1.0.2 - Now based on John Eberle's custom Get_Stats function. 
+Now deals properly with empty arrays by displaying a disabled zero button such as when there are no pending installs or no failed installs.
+Changed the Button urls to get to appropriate items. Edited the button labels to standardize capitalization.
+Tested with Machine Group filtering.
+* January 7, 2019 Version 1.0.1 - Some bug fixes to pending and data calls.
 * January 7, 2019 Version 1.0
- * January 7, 2019 Version 1.0.1 - Some bug fixes to pending and data calls.
 
 ## Contributors
 * Alex Narvey
+* John Evberle
+* Arjen van Bochoven
 
 —
 Alex Narvey
--- munki_stats_widget.php
+++ munki_stats_widget.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-md-6">
     <div class="panel panel-default" id="munki_stats-widget">
-        <div id="munki_stats-widget" class="panel-heading" data-container="body" data-i18n="[title]munki_stats_widget.title">
+        <div id="munki_stats-widget" class="panel-heading" data-container="body" data-i18n="[title]managedinstalls.widget.munki_stats.title">
             <h3 class="panel-title"><i class="fa fa-check-square-o"></i> 
                 <span data-i18n="munki_stats_widget.title">Managed Software Stats</span>
                 <list-link data-url="/show/listing/munkireport/munki"></list-link>
@@ -13,53 +13,63 @@
 <script>
 $(document).on('appUpdate', function(e, lang) {
 
-		//Set the hours to one month
-		var hours = 24 * 30;
-		//Gather the data from ManagedInstalls Module
-		var myObj,  x = "";
-		myObj = $.getJSON( appUrl + '/module/managedinstalls/get_stats/'+hours, function( data ) {
+        //Gather the data from ManagedInstalls Module (thanks to John Eberle for providing custom getStats function)
+        //One month = 720 hours
+        var myObj,  x = "";
+        myObj = $.getJSON( appUrl + '/module/managedinstalls/get_stats/'+720, function( data ) {
 
         if(data.error){
-        //alert(data.error);
-        return;
+            //alert(data.error);
+            return;
         }
 
         var panel = $('#munki_stats-widget div.panel-body'),
         baseUrl = appUrl + '/show/listing/munkireport/munki';
         panel.empty();
         
-        //pending total;
-        var munkiPending = Number(data[5].total_items);
-        var applePending = Number(data[4].total_items);
-		var totalPending = (munkiPending + applePending);
-                
+
+		//If pending_installs_munki array is missing use null to provide numerical zero
+		 if (data['pending_install_munki']==null) {
+         var munkiPending =  0 ;
+         } else {
+         var munkiPending = Number(data['pending_install_munki'].total_items);
+         }
+		//If pending_installs_applesus array is missing use null to provide numerical zero
+         if (data['pending_install_applesus']==null) {
+         var applePending =  0  ;
+         } else {
+         var applePending = Number(data['pending_install_applesus'].total_items);
+         }
+		//Get pending install total;
+		 var totalPending = (munkiPending.valueOf() + applePending.valueOf());
+		
+		
         // Set blocks, disable if zero
-        if(data[3].clients != "0"){
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-info"><span class="bigger-150">'+data[3].clients+'</span><br>'+i18n.t('Clients')+'</a>');
+        if(data['installed_munki'].clients!=null){
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-info"><span class="bigger-150">'+data['installed_munki'].clients+'</span><br>'+i18n.t('client.clients')+'</a>');
         } else {
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-info disabled"><span class="bigger-150">'+data[3].clients+'</span><br>'+i18n.t('Clients')+'</a>');
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-info disabled"><span class="bigger-150">'+data['installed_munki'].clients+'</span><br>'+i18n.t('client.clients')+'</a>');
         }
-        if(data[3].total_items != "0"){
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-success"><span class="bigger-150">'+data[3].total_items+'</span><br>'+i18n.t('Installs')+'</a>');
+
+        if(data['installed_munki'].total_items!=null){
+            panel.append(' <a href="https://munkireport.precursor.ca/index.php?/module/managedinstalls/listing/#installed" class="btn btn-success"><span class="bigger-150">'+data['installed_munki'].total_items+'</span><br>'+i18n.t('Installs')+'</a>');
         } else {
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-success disabled"><span class="bigger-150">'+data[3].total_items+'</span><br>'+i18n.t('Installs')+'</a>');
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-success disabled"><span class="bigger-150">'+data['installed_munki'].total_items+'</span><br>'+i18n.t('Installs')+'</a>');
         }
 
         if(totalPending != "0"){
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-warning"><span class="bigger-150">'+totalPending+'</span><br>'+i18n.t('Pending')+'</a>');
+        	panel.append(' <a href="https://munkireport.precursor.ca/index.php?/module/managedinstalls/listing/#pending_install" class="btn btn-warning"><span class="bigger-150">'+totalPending+'</span><br>'+i18n.t('pending')+'</a>');
         } else {
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-warning disabled"><span class="bigger-150">'+totalPending+'</span><br>'+i18n.t('Pending')+'</a>');
-        }
-
-       if(data[0].total_items != "0"){
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-danger"><span class="bigger-150">'+data[0].total_items+'</span><br>'+i18n.t('Failed')+'</a>');
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-warning disabled"><span class="bigger-150">'+"0"+'</span><br>'+i18n.t('pending')+'</a>');
+         }
+        
+        if(data['install_failed_munki']!=null){
+            panel.append(' <a href="https://munkireport.precursor.ca/index.php?/module/managedinstalls/listing/#install_failed" class="btn btn-danger"><span class="bigger-150">'+data['install_failed_munki'].total_items+'</span><br>'+i18n.t('failed')+'</a>');
         } else {
-            panel.append(' <a href="'+baseUrl+'" class="btn btn-danger disabled"><span class="bigger-150">'+data[0].total_items+'</span><br>'+i18n.t('Failed')+'</a>');
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-danger disabled"><span class="bigger-150">'+"0"+'</span><br>'+i18n.t('failed')+'</a>');
         }
-
 
     });
-
 });
 
 </script>
